bd352413fbf6ed019e6cfdaef27d9299fae58bdc
violet <evelgaden@gmail.com>
2023-04-27 00:51:40 +0800

    Change to the compute float value. Step01

--- C++/Compute.cpp
+++ C++/Compute.cpp
@@ -10,8 +10,8 @@ void ConvertEnd(const string & front, vector<char> & end);
 int Compute(const vector<char> & end);
 bool IsOp(char ch);
 bool IsBig(char ch0, char ch1);
-int FindInt(int & value, const vector<char> & arr, int star);
-int TopInt(stack<int> & s);
+int FindValue(int & value, const vector<char> & arr, int star);
+int TopFloat(stack<int> & s);
 int SigComp(int x, int y, char op);
 
 int main() {
@@ -22,11 +22,11 @@ int main() {
     cout << "Enter the expression: ";
     while (getline(cin, front))
 
-    {// Conver to the end format
+    // Conver to the end format
     ConvertEnd(front, end);
 
     // Compute the value
-    cout << Compute(end) << endl;}    
+    cout << Compute(end) << endl;
 
     return 0;
 }
@@ -84,15 +84,15 @@ bool IsBig(char ch0, char ch1) {
 
 int Compute(const vector<char> & end) {
     // Compute the value throught the end format
-    stack<int> s;
+    stack<float> s;
 
     for (int i = 0; i < end.size(); i++) {
         if (isdigit(end[i])) {
-            int temp = 0;
-            i += FindInt(temp, end, i);
+            float temp = 0;
+            i += FindValue(temp, end, i);
             s.push(temp);
         } else if (IsOp(end[i])) {
-            s.push(SigComp(TopInt(s), TopInt(s), end[i]));
+            s.push(SigComp(TopFloat(s), TopFloat(s), end[i]));
         }
     }
 
@@ -101,17 +101,26 @@ int Compute(const vector<char> & end) {
     } else cout << "Error!\n";
 }
 
-int FindInt(int & value, const vector<char> & arr, int start) {
-    int i; value = 0;
+int FindValue(float & value, const vector<char> & arr, int start) {
+    int i, j; value = 0;
+    bool isDecimal = false;
+    float integer = 0, decimal = 0;
 
-    for (i = start; i < arr.size() && isdigit(arr[i]); i++) {
-        value = (value * 10) + (arr[i] - '0');         
+    for (i = start; i < arr.size() && (isdigit(arr[i]) || arr[i] == '.'); i++) {
+        integer = (integer * 10) + (arr[i] - '0');         
+        if (arr[i] == '.') { isDecimal = true; break; }
     }
+    if (isDecimal == true) {
+        for (j = i + 1; j < arr.size() && isdigit(arr[i]); j++) {
+            
+        }
+    }
+    value = integer + decimal;
 
     return i - start - 1;
 }
 
-int TopInt(stack<int> & s) {
+int TopFloat(stack<int> & s) {
     if (s.empty()) {
         cout << "Error!\n";
         return 0;
